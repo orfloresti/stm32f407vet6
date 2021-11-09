75cb94688db4d664d5c39111af8742b8f94ce78a
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2021-11-08 20:35:41 -0600

    Add blink in stm32f411ceu6 using bare metal and structs

---
+++ stm32cubeide/host/pointers/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	char a1 = 'A';
+
+	printf("Address of variable a1: %p\n", &a1);
+	return 0;
+}
--- stm32cubeide/stm32f411ceu6/hal-stm32f411ceu6-blink/Core/Src/main.c
+++ stm32cubeide/stm32f411ceu6/hal-stm32f411ceu6-blink/Core/Src/main.c
@@ -95,7 +95,7 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-	HAL_Delay(500);
+	HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
