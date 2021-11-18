12d41f607ae2a37b78786ffc144a50239cd5367e
orflores <orflores@rassini.com>
2021-11-18 08:34:53 -0600

    Example for stm32f411ceu6 BSRR

--- stm32cubeide/nucleo-l476rg/cmsis-nucleo-l476rg-blink/Core/Src/main.c
+++ stm32cubeide/nucleo-l476rg/cmsis-nucleo-l476rg-blink/Core/Src/main.c
@@ -18,8 +18,8 @@ void systickDelayMs( int ms ) {
 
 	// Clock seems to be 2000Mhz
 	SysTick->LOAD = 2000; // Reload with number of clocks per milliseconds
-	SysTick->VAL = 0;		// Cleat current value
-	SysTick->CTRL = 5; 	// Enable Systick
+	SysTick->VAL = 0;		// Clean current value
+	SysTick->CTRL = 5; 	// Enable SysTick
 
 	for( int i= 0; i < ms; i++ ) {
 		// Wait until the counter flag is set
