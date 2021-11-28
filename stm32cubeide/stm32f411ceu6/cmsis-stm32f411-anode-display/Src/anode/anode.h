7f27ea1baeb7545fb2b1e95e6861fbdf69e1961d
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2021-11-27 20:40:34 -0600

    Add a header to control the each display

---
+++ stm32cubeide/stm32f411ceu6/cmsis-stm32f411-anode-display/Src/anode/anode.h
@@ -0,0 +1,8 @@
+#ifndef ANODE_H // include guard
+#define ANODE_H
+
+void anodeInitPort();
+void setDisplay(int number, int display);
+
+#endif
+
--- stm32cubeide/stm32f411ceu6/cmsis-stm32f411-anode-display/Src/main.c
+++ stm32cubeide/stm32f411ceu6/cmsis-stm32f411-anode-display/Src/main.c
@@ -10,84 +10,17 @@
  * */
 
 #include "stm32f4xx.h"
-
-
-#define GPIOAEN		(1U<<0)
-
-void timer() {
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-	for(int i=0; i<0xffffU; i++){}
-}
+#include "./anode/anode.h"
 
 int main(void) {
 
-	// Enable clock for GPIOC
-	RCC->AHB1ENR |= GPIOAEN;
-
-	// Mode GPIOA 0-15 as general output
-	GPIOA->MODER |= (0x5555U << 0);
+	anodeInitPort();
 
 	while(1) {
-
-		// Set 0
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x3fU << 0);
-		timer();
-
-		// Set 1
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x6U << 0);
-		timer();
-
-		// Set 2
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x5bU << 0);
-		timer();
-
-		// Set 3
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x4fU << 0);
-		timer();
-
-		// Set 4
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x66U << 0);
-		timer();
-
-		// Set 5
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x6dU << 0);
-		timer();
-
-		// Set 6
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x7dU << 0);
-		timer();
-
-		// Set 7
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x7U << 0);
-		timer();
-
-		// Set 7
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x7fU << 0);
-		timer();
-
-		// Set 7
-		GPIOA->ODR = (0x0U << 0);
-		GPIOA->ODR |= ~(0x6fU << 0);
-		timer();
+		setDisplay(8, 1);
+		setDisplay(7, 2);
+		setDisplay(2, 3);
+		setDisplay(4, 4);
 	}
 }
 
